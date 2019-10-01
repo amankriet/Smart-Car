abebdd8d79a4b894e5142975944ed9fdc85414d9
Aman Kumar <amankriet@gmail.com>
2019-10-01 23:36:47 +0530

    Arduino programs for hc06 bluetooth setup, android device control and ultrasonic control

---
+++ AndroidDevice.ino
@@ -0,0 +1,96 @@
+int izqA = 7;
+int izqB = 6;
+int derA = 5;
+int derB = 4;
+int vel = 200; // Velocidad de los motores (0-255)
+int estado = 'g'; // inicia detenido
+
+void setup() {
+Serial.begin(9600); // inicia el puerto serial para comunicacion con el Bluetooth
+pinMode(derA, OUTPUT); 
+pinMode(derB, OUTPUT);
+pinMode(izqA, OUTPUT);
+pinMode(izqB, OUTPUT);
+}
+
+void loop() {
+
+  
+if(Serial.available()>0){ // lee el bluetooth y almacena en estado
+estado = Serial.read();
+}
+if(estado=='a'){ //Forward
+  Serial.println(estado);
+digitalWrite(derB, 0);
+digitalWrite(izqB, 0);
+digitalWrite(derA, vel);
+digitalWrite(izqA, vel);
+}
+if(estado=='d'){ // Right
+  Serial.println(estado);
+digitalWrite(derB, vel);
+digitalWrite(izqB, 0);
+digitalWrite(derA, 0);
+digitalWrite(izqA, vel);
+}
+if(estado=='c'){ // Stop
+  Serial.println(estado);
+digitalWrite(derB, 0);
+digitalWrite(izqB, 0);
+digitalWrite(derA, 0);
+digitalWrite(izqA, 0);
+}
+if(estado=='b'){ // left
+  Serial.println(estado);
+digitalWrite(derB, 0);
+digitalWrite(izqB, vel);
+digitalWrite(izqA, 0);
+digitalWrite(derA, vel);
+}
+if(estado=='e'){ // Reverse
+  Serial.println(estado);
+digitalWrite(derA, 0);
+digitalWrite(izqA, 0);
+digitalWrite(derB, vel); 
+digitalWrite(izqB, vel);
+}
+if(estado=='f'){ // Boton ON se mueve sensando distancia
+digitalWrite(izqB, vel);
+digitalWrite(izqA, vel);
+digitalWrite(derA, vel);
+digitalWrite(derB, vel);
+
+}
+if(estado=='g'){ // Boton OFF, deteine los motores no hace nada
+digitalWrite(izqB, 0);
+digitalWrite(izqB, 0);
+digitalWrite(derB, 0);
+digitalWrite(derB, 0);
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ UltrasonicSensorControl.ino
@@ -0,0 +1,147 @@
+#include <NewPing.h>
+#include <Servo.h>
+
+#define TRIG_PIN A2
+#define ECHO_PIN A3
+#define MAX_DISTANCE 200
+NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
+Servo myservo;
+
+boolean goesForward=false;
+int distance = 100;
+int speedSet = 0;
+
+const int motorPin1 =7;
+const int motorPin2 =6;
+//Motor B
+const int motorPin3 =5;
+const int motorPin4 =4;
+
+void setup()
+{
+  
+  myservo.attach(9);
+  myservo.write(115);
+  delay(2000);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  distance = readPing();
+  delay(100);
+  }
+  void loop ()
+  {
+    int distanceR =0;
+    int distanceL =0;
+    delay(40);
+    
+    if(distance<=35)
+    {
+      moveStop();
+      delay(100);
+      moveBackward();
+      delay(300);
+      moveStop();
+      delay(200);
+      distanceR = lookRight();
+      delay(200);
+      distanceL = lookLeft();
+      delay(200);
+      
+      if(distanceR>=distanceL)
+      {
+        turnRight();
+        turnRight();
+        moveStop();
+      }
+      else
+      {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+        moveStop();
+      }
+    }
+    else
+    {
+      moveForward();
+    }
+    distance = readPing();
+  }
+  
+  int lookRight()
+  {
+    myservo.write(40);
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(115);
+    return distance;
+  }
+  int lookLeft()
+  {
+    myservo.write(180);
+    delay(500);
+    int distance = readPing();
+    delay(100);
+    myservo.write(115);
+    return distance;
+    delay(100);
+  }
+  
+  int readPing(){
+  delay(70);
+  int cm = sonar.ping_cm();
+  if(cm==0)
+  {
+    cm=250;
+  }
+  return cm;
+}
+void moveStop()
+{
+  analogWrite(motorPin1,0);
+  analogWrite(motorPin2,0);
+  analogWrite(motorPin3,0);
+  analogWrite(motorPin4,0);
+}
+
+void moveForward()
+{
+  analogWrite(motorPin1,150);
+  analogWrite(motorPin2,0);
+  analogWrite(motorPin3,150);
+  analogWrite(motorPin4,0);
+}
+
+void moveBackward()
+{
+  
+  analogWrite(motorPin1,0);
+  analogWrite(motorPin2,150);
+  analogWrite(motorPin3,0);
+  analogWrite(motorPin4,150);
+}
+
+void turnRight()
+{
+  analogWrite(motorPin1,150);
+  analogWrite(motorPin2,0);
+  analogWrite(motorPin3,0);
+  analogWrite(motorPin4,150);
+  delay(300);
+  moveForward();
+}
+
+void turnLeft()
+{
+  analogWrite(motorPin1,0);
+  analogWrite(motorPin2,150);
+  analogWrite(motorPin3,150);
+  analogWrite(motorPin4,0);
+  delay(300);
+  moveForward();
+}
